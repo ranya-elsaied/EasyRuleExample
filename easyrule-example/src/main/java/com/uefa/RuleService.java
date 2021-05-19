a2252cd3f0b0052c59b5a5deef4db30fabb74a01
ranya-elsaied <ranya.elsaied@gmail.com>
2021-05-19 12:39:42 +0200

    Add some rules and the DTO to hold the rule conditions

--- easyrule-example/src/main/java/com/uefa/RuleService.java
+++ easyrule-example/src/main/java/com/uefa/RuleService.java
@@ -1,3 +1,46 @@
+package com.uefa;
+
+
+import com.uefa.dto.RuleDto;
+import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rule;
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.api.RulesEngine;
+import org.jeasy.rules.mvel.MVELRule;
+import org.springframework.stereotype.Service;
+import org.jeasy.rules.core.*;
+
+@Service
 public class RuleService {
+
+    public static void runRules() {
+
+        //https://github.com/imona/tutorial/wiki/MVEL-Guide#foreach
+
+        Facts values = new Facts();
+        values.put("abc", new RuleDto("name1", "10 >5"));
+        values.put("abd", new RuleDto("name1", "5 >5"));
+
+        Rule rule = new RuleBuilder()
+                .name("myRule")
+                .description("myRuleDescription")
+                .description("if it rains then take an umbrella")
+                .when(facts -> ((RuleDto) facts.get("abc")).getName().equals("name1"))
+                .then(facts -> System.out.println("valid rule 1"))
+                .build();
+
+        MVELRule weatherRule = new MVELRule()
+                .name("weather rule")
+                .description("if it rains then take an umbrella")
+                .when(((RuleDto) values.getFact("abd").getValue()).getCondition())
+                .then("System.out.println(\"valid rule 2\");");
+
+        Rules rules = new Rules();
+        rules.register(weatherRule);
+        rules.register(rule);
+        RulesEngine rulesEngine = new DefaultRulesEngine();
+        rulesEngine.fire(rules, values);
+
+    }
 
 }
---
+++ easyrule-example/src/main/java/com/uefa/dto/RuleDto.java
@@ -0,0 +1,18 @@
+package com.uefa.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RuleDto {
+
+    private String name;
+    private String condition;
+
+}
