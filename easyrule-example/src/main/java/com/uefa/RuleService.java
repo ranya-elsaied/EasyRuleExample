00bb26f5e89bcc911da26cce321c7ea4725b8adb
ranya-elsaied <ranya.elsaied@gmail.com>
2021-06-14 12:04:26 +0200

    Add add more rule varieties

--- easyrule-example/src/main/java/com/uefa/RuleService.java
+++ easyrule-example/src/main/java/com/uefa/RuleService.java
@@ -2,6 +2,7 @@ package com.uefa;
 
 
 import com.uefa.dto.RuleDto;
+import org.jeasy.rules.api.Fact;
 import org.jeasy.rules.api.Facts;
 import org.jeasy.rules.api.Rule;
 import org.jeasy.rules.api.Rules;
@@ -9,6 +10,8 @@ import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.mvel.MVELRule;
 import org.springframework.stereotype.Service;
 import org.jeasy.rules.core.*;
+
+import java.util.Iterator;
 
 @Service
 public class RuleService {
@@ -19,28 +22,70 @@ public class RuleService {
 
         Facts values = new Facts();
         values.put("abc", new RuleDto("name1", "10 >5"));
-        values.put("abd", new RuleDto("name1", "5 >5"));
+        values.put("abd", new RuleDto("name1", "(7 >5) &&(9<8)"));
 
-        Rule rule = new RuleBuilder()
-                .name("myRule")
-                .description("myRuleDescription")
-                .description("if it rains then take an umbrella")
-                .when(facts -> ((RuleDto) facts.get("abc")).getName().equals("name1"))
-                .then(facts -> System.out.println("valid rule 1"))
+
+        Rule rule1 = new RuleBuilder()
+                .name("rule1")
+                .description("desc1")
+                .when(facts -> ((RuleDto) facts.get("abc")).getName().equals("name2"))
+                .then(facts -> System.out.println("valid rule 1" + facts.get("abc").toString()))
                 .build();
 
-        MVELRule weatherRule = new MVELRule()
-                .name("weather rule")
-                .description("if it rains then take an umbrella")
+        MVELRule rule2 = new MVELRule()
+                .name("rule2")
+                .description("desc2")
                 .when(((RuleDto) values.getFact("abd").getValue()).getCondition())
                 .then("System.out.println(\"valid rule 2\");");
 
+        Facts data = new Facts();
+        data.put("x", 10);
+
+        MVELRule rule3 = new MVELRule()
+                .name("rule3")
+                .description("desc3")
+                .when("x > 6")
+                .then("System.out.println(\"valid rule 3\");");
+
+
         Rules rules = new Rules();
-        rules.register(weatherRule);
-        rules.register(rule);
+        rules.register(rule1);
+        rules.register(rule2);
+
         RulesEngine rulesEngine = new DefaultRulesEngine();
         rulesEngine.fire(rules, values);
 
+        rules = new Rules();
+        rules.register(rule3);
+
+        rulesEngine = new DefaultRulesEngine();
+        rulesEngine.fire(rules, data);
+
+
+        useMoreThanOneFact(rulesEngine);
+
+    }
+
+    private static void useMoreThanOneFact(RulesEngine rulesEngine) {
+        Rules rules;
+        Facts otherValues = new Facts();
+        otherValues.put("value1", "2");
+        otherValues.put("value2", "10");
+        RuleDto dto = new RuleDto("ab", "6 >");
+
+        rules = new Rules();
+        Iterator<Fact<?>> it = otherValues.iterator();
+        while (it.hasNext()) {
+            Fact fact = it.next();
+            MVELRule otherRule = new MVELRule()
+                    .name("other rule" + fact.getName())
+                    .description("if true then print")
+                    .when(dto.getCondition() + fact.getName())
+                    .then("System.out.println(\"valid rule 4 " + fact.getName() + "\");");
+            rules.register(otherRule);
+        }
+
+        rulesEngine.fire(rules, otherValues);
     }
 
 }
